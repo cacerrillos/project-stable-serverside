6c5ad4af236a63344dbbebde7a6ec1fea7acd9ae
Carlos Cerrillos <ccerrillos@climatec.com>
2018-05-08 19:58:43 -0700

    Library updates

--- ProjectStableLibrary/Date.cs
+++ ProjectStableLibrary/Date.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 
 namespace ProjectStableLibrary {
 	[Table("dates")]
@@ -14,6 +17,22 @@ namespace ProjectStableLibrary {
 		}
 		public override string ToString() {
 			return $"date: {date}";
+		}
+
+		public static async Task<List<Date>> GetDates(AmazonDynamoDBClient client) {
+			List<Date> list = new List<Date>();
+
+			var dates = await client.ScanAsync(new ScanRequest() {
+				TableName = "endearing-otter-dates"
+			});
+
+			foreach(var date in dates.Items) {
+				list.Add(new Date() {
+					date = uint.Parse(date["date"].N)
+				});
+			}
+
+			return list;
 		}
 	}
 }
