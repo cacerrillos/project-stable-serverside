9f6ce55a308bc3d877892c368ef4c39308637459
Carlos Cerrillos <carlos@cerrillostech.com>
2018-05-08 20:27:29 -0700

    Library updates

--- ProjectStableLibrary/Grade.cs
+++ ProjectStableLibrary/Grade.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ProjectStableLibrary {
 	[Table("grades")]
@@ -20,6 +24,27 @@ namespace ProjectStableLibrary {
 		public uint default_amount {
 			get;
 			set;
+		}
+
+		public static async Task<Dictionary<uint, Grade>> GetGrades(AmazonDynamoDBClient client) {
+			Dictionary<uint, Grade> response = new Dictionary<uint, Grade>();
+
+			var grades = await client.ScanAsync(new ScanRequest() {
+				TableName = "endearing-otter-grades"
+			});
+
+			foreach(var grade in grades.Items) {
+				var g = new Grade() {
+					grade_id = uint.Parse(grade["grade_id"].N),
+					grade_name = grade["grade_name"].S,
+					default_amount = uint.Parse(grade["default_amount"].N)
+				};
+
+				response.Add(g.grade_id, g);
+			}
+			
+
+			return response;
 		}
 	}
 }
